d8502709abc7fbc739f79c647fe4d9c160d1fd52
Danarendra <narendra.daniswara16@gmail.com>
2024-12-09 13:52:50 +0700

    Hello!

--- CaseStudy.java
+++ CaseStudy.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 public class CaseStudy {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(" ==PENCATATAN PRESTASI MAHASISWA== ");
-        System.out.println("1. Tambah Data Prestasi");
-        System.out.println("2. Tampilkan Semua Prestasi");
-        System.out.println("3. Analisis Prestasi Berdasarkan Jenis");
-        System.out.println("4. Keluar");
-        System.out.print("Pilih Menu: ");
-        int menu = input.nextInt();
+        System.out.println("Hello world");
     }
 }
